90469a5ddabf19a49f6244cb3760f4fc7df91152
Joshua Newman <joshuajmnewman@gmail.com>
2018-08-09 09:48:43 +0100

    Update FilmRentalService.sol
    Added a "Service" interface

--- contracts/FilmRentalService.sol
+++ contracts/FilmRentalService.sol
@@ -1,13 +1,17 @@
 pragma solidity 0.4.24;
 
 
+import "./Service.sol";
+
+
 /**
  * Simple example service that returns the URL to a film
  */
-contract FilmRentalService {
+contract FilmRentalService is Service {
 
 
     function getFromService() external pure returns (string) {
         return "www.getMyFilm.com/logan";
     }
 }
+
